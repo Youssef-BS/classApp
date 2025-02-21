c3edcb6c90343889b2d5c6fa0480f8f02c49002b
unknown <youssef-BS>
2025-02-21 11:49:30 +0100

    add sass and add test for sass

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 import Event from "./pages/events/Event";
+import "./test.scss";
 
 function Layout({ children }) {
   return (
@@ -16,13 +17,23 @@ function Layout({ children }) {
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Event />} />
-        </Routes>
-      </Layout>
-    </Router>
+    // <Router>
+    //   <Layout>
+    //     <Routes>
+    //       <Route path="/" element={<Event />} />
+    //     </Routes>
+    //   </Layout>
+    // </Router>
+    <div class="profile-card">
+  <div class="profile-img">
+    <img src="https://i.pinimg.com/736x/fa/d5/e7/fad5e79954583ad50ccb3f16ee64f66d.jpg" alt="Photo de profil" />
+  </div>
+  <div class="profile-info">
+    <h2>Student</h2>
+    <p>UI/UX Designer & Frontend Developer</p>
+    <button class="btn-primary">Contact</button>
+  </div>
+</div>
   );
 }
 
